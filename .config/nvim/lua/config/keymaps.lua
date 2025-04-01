f3a76efda8d2e5fcf9c16c9a30da0bbcbfcf1e12
Cristobal <cristobalriaga@gmail.com>
2025-03-31 20:08:21 -0500

    HmmmmMmMm

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<Leader>fp", "<cmd>let @+ = @%<CR>", { desc = "Copy absolute path" })
+vim.keymap.set("n", "<Leader>fp", "<cmd>let @+ = expand('%:p')<CR>", { desc = "Copy absolute path" })
+vim.keymap.set("n", "<leader>fi", function()
+  vim.notify(vim.fn.system("ls -l " .. vim.fn.expand("%")))
+end, { desc = "File information" })
 -- vim.keymap.set("n", "<Leader>yx", "<cmd>let @+ = expand('<cfile>')<CR>", { desc = "Copy path/link" })
 vim.keymap.del("n", "<C-Down>")
 vim.keymap.del("n", "<C-Up>")
