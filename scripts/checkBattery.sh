0c2040cda5d1cadf8b161a8dab68e2cdbfba4c1b
Cristobal <cristobalriaga@gmail.com>
2025-04-03 21:47:12 -0500

    Hehe

--- scripts/checkBattery.sh
+++ scripts/checkBattery.sh
@@ -1,10 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
   read -r status capacity
 
-  if [ "$status" = Discharging ] && [ "$capacity" -le 10 ]; then
-    ~/scripts/onLowBattery.sh
-  elif [ "$status" = Charging ] && [ "$capacity" -ge 100 ]; then
-    ~/scripts/onFullBattery.sh
+  if [ "$status" = Charging ]; then
+    rm -f ~/scripts/.10BatteryNotif
+    rm -f ~/scripts/.40BatteryNotif
+  elif [ "$status" = Discharging ]; then
+    rm -f ~/scripts/.100BatteryNotif
+    rm -f ~/scripts/.60BatteryNotif
+  fi
+
+  if [ "$status" = Discharging ] && [ "$capacity" -le 10 ] && [ ! -f ~/scripts/.10BatteryNotif ]; then
+    notify-send -u critical -a "Power Management (user)" "Low Battery" "Plug your computer to avoid losing any work."
+    touch ~/scripts/.10BatteryNotif
+    mpv --loop=1 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
+  elif [ "$status" = Discharging ] && [ "$capacity" -le 40 ] && [ ! -f ~/scripts/.35BatteryNotif ]; then
+    notify-send -u critical -a "Power Management (user)" "Battery At 40%" "Plug your computer."
+    touch ~/scripts/.40BatteryNotif
+    mpv /usr/share/sounds/ocean/stereo/outcome-failure.oga
+  elif [ "$status" = Charging ] && [ "$capacity" -ge 60 ] && [ ! -f ~/scripts/.60BatteryNotif ]; then
+    notify-send -u critical -a "Power Management (user)" "Battery At 60%" "Unplug your computer."
+    touch ~/scripts/.60BatteryNotif
+    mpv /usr/share/sounds/ocean/stereo/outcome-success.oga
+  elif [ "$status" = Charging ] && [ "$capacity" -ge 100 ] && [ ! -f ~/scripts/.100BatteryNotif ]; then
+    notify-send -u normal -a "Power Management (user)" "Fully Charged" "You may unplug your computer."
+    touch ~/scripts/.100BatteryNotif
+    mpv /usr/share/sounds/ocean/stereo/message-sent-instant.oga
   fi
 }
--- scripts/onLowBattery.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-notify-send -u critical -a "Power Management (user)" "Low Battery" "Plug your computer to avoid losing any work."
-mpv --loop=1 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
