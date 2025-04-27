3c2b615998c016a27ef1a278fd50bf186e57cef9
Cristobal <cristobalriaga@gmail.com>
2025-04-26 23:56:43 -0500

    hypr: wip support for numlock, toggle layout binding, disabled binding to stop hyprland

---
+++ scripts/hypr/numlock.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# # -o abg:d:
+# VALID_ARGS=$(getopt --long set:,get -- "$@")
+# # if ! VALID_ARGS; then
+# if [[ $? -ne 0 ]]; then
+#   exit 1
+# fi
+#
+# eval set -- "$VALID_ARGS"
+# while [ : ]; do
+while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do
+  case $1 in
+  --get) # -b | --beta)
+    if [[ -e ~/scripts/hypr/.numlockOn ]]; then
+      echo true
+    else
+      echo false
+    fi
+    exit
+    ;;
+  --set)
+    if [ "$1" = "true" ]; then
+      touch ~/scripts/hypr/.numlockOn
+    else
+      rm ~/scripts/hypr/.numlockOn
+    fi
+    exit
+    ;;
+  --toggle)
+    if [[ -e ~/scripts/hypr/.numlockOn ]]; then
+      rm ~/scripts/hypr/.numlockOn
+    else
+      touch ~/scripts/hypr/.numlockOn
+    fi
+    exit
+    ;;
+  esac
+done
+if [[ "$1" == '--' ]]; then shift; fi
---
+++ scripts/hypr/toggleLayout.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+layout=$(hyprctl -j getoption general:layout | jq --raw-output '.str')
+echo "$layout"
+if [[ $layout = "dwindle" ]]; then
+  hyprctl keyword general:layout master
+else
+  hyprctl keyword general:layout dwindle
+fi
