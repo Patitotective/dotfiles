0104e0421038c2fc692bdf5d04ee5a54455e0cbf
Cristobal <cristobalriaga@gmail.com>
2025-04-26 22:42:55 -0500

    nvim: enabled mousemoveevent to only show close buffer close button on hover, fixed bufferline cycle bug

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,6 +3,7 @@
 -- Add any additional options here
 vim.opt.shada = "!,'10000,<100,s10,h" -- Increase the number of oldfiles (first number) and stored lines for registers
 vim.o.shell = "/usr/bin/fish"
+vim.o.mousemoveevent = true
 vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
   filename = { ["env-hyprland"] = "conf" },
--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,17 +1,61 @@
+local function repeatCmd(cmd, times)
+  for _ = 1, times do
+    vim.cmd(cmd)
+  end
+end
+
 return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
       indicator = {
         style = "none",
+        -- icon = " ▐",
       },
       -- separator_style = "thin",
       -- show_close_icon = false,
-      -- hover = {
-      --   enabled = true,
-      --   delay = 200,
-      --   reveal = { "close" },
-      -- },
+      hover = {
+        enabled = true,
+        delay = 00,
+        reveal = { "close" },
+      },
+      -- Don't use until relative numbers are a feature https://github.com/akinsho/bufferline.nvim/issues/249
+      -- numbers = function(opts)
+      --   return string.format("%s|", opts.ordinal)
+      -- end,
+    },
+  },
+  keys = {
+    {
+      "L",
+      function()
+        repeatCmd("BufferLineCycleNext", vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "H",
+      function()
+        -- vim.cmd("bprev " .. vim.v.count1)
+        repeatCmd("BufferLineCyclePrev", vim.v.count1)
+      end,
+      desc = "Previous buffer",
+    },
+    {
+      "]b",
+      function()
+        -- vim.cmd("bnext " .. vim.v.count1)
+        repeatCmd("BufferLineCycleNext", vim.v.count1)
+      end,
+      desc = "Next buffer",
+    },
+    {
+      "[b",
+      function()
+        -- vim.cmd("bprev " .. vim.v.count1)
+        repeatCmd("BufferLineCyclePrev", vim.v.count1)
+      end,
+      desc = "Previous buffer",
     },
   },
 }
