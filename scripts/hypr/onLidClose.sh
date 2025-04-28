e310779f65bb517573615efe460d0cecd1974909
Cristobal <cristobalriaga@gmail.com>
2025-04-28 11:01:20 -0500

    hypr: remove unneeded debugging

--- scripts/hypr/onLidClose.sh
+++ scripts/hypr/onLidClose.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ $(hyprctl -j monitors all | jq length) -eq 1 ]]; then
-  echo "before: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
+  # echo "before: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
   ~/scripts/hypr/lock.sh
-  echo "after lock: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
+  # echo "after lock: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
   ~/scripts/hypr/suspend.sh
-  echo "after suspend: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
+  # echo "after suspend: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
 fi
