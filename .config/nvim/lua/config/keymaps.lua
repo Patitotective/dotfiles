aa2bf6fcb6ee25685f7fa94d0b86ddb72647ac01
Cristobal <cristobalriaga@gmail.com>
2025-04-27 20:33:14 -0500

    kitty, nvim: fixed ctrl-v mapped in normal nvim mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -33,16 +33,19 @@ vim.keymap.set({ "i" }, "<c-f>", "<c-o>w") -- next word
 
 vim.keymap.set({ "i" }, "<c-z>", "<c-d>") -- deindent
 
-vim.keymap.set({ "i" }, "<c-u>", "<c-o>d0") -- del until start of line
+-- vim.keymap.set({ "i" }, "<c-u>", "<c-o>d0") -- del until start of line
 vim.keymap.set({ "i" }, "<c-c>", "<c-o>d$") -- del until end of line
 vim.keymap.set({ "i", "c" }, "<c-s>", "<bs>") -- del prev char
 vim.keymap.set({ "i", "c" }, "<c-d>", "<del>") -- del next char
 
 vim.keymap.set({ "i" }, "<c-x>", "<c-o>de") -- del next word
 
+-- vim.keymap.set({ "i" }, "<c-i>", "<c-o>O")
+-- vim.keymap.set({ "i" }, "<c-m>", "<c-o>o")
+
 -- vim.keymap.set({ "i" }, "<c-w>", "<c-o>dvb", { noremap = true }) -- del prev word (case_Wise)
 
--- vim.keymap.set({ "i", "c" }, "<c-v>", "<c-r>+") -- paste
+vim.keymap.set({ "i", "c" }, "<c-v>", "<c-r>+") -- paste
 
 -- vim.keymap.set({ "i" }, "<c-m>", "<c-o>O") -- insert line before
 -- vim.keymap.set({ "i" }, "<cr>", "<c-o>o") -- insert line after
