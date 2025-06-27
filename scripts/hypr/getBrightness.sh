48d457ca787ffbac6e548bd1e196dbb099609b24
Cristobal <cristobalriaga@gmail.com>
2025-06-27 17:42:22 -0500

    scripts: removed old scripts

--- scripts/hypr/getBrightness.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-raw_brightness="$(busctl --user -- get-property rs.wl-gammarelay / rs.wl.gammarelay Brightness)" # d 0.5
-brightness="$(echo "$raw_brightness" | cut -d' ' -f2)"                                           # 0.5
-echo "$brightness"
