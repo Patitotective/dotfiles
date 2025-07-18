6ce1f2b73c142737044a8960dffe5374f5b879e7
Cristobal <cristobalriaga@gmail.com>
2025-07-17 21:49:05 -0500

    hypr: fixed $mainMod+g toggling group instead of always enabling it

---
+++ scripts/hypr/enablegroup.fish
@@ -0,0 +1,6 @@
+#!/bin/fish
+# If the active window is in a group, do nothing, otherwise make it into a group
+set groupedLen (hyprctl -j activewindow | jq '.grouped | length')
+if test $groupedLen -eq 0
+    hyprctl dispatch togglegroup
+end
