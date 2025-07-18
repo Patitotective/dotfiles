478060b6f34e85cdd985e4932e9ed844a685584f
Cristobal <cristobalriaga@gmail.com>
2025-07-17 19:04:05 -0500

    scripts/hypr/onLidClose: logs not needed anymore

--- scripts/hypr/onLidClose.sh
+++ scripts/hypr/onLidClose.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-echo "0: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
 if [[ $(hyprctl -j monitors all | jq length) -eq 1 ]]; then
-  echo "1: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
   ~/scripts/hypr/lock.sh
-  echo "2: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
   ~/scripts/hypr/suspend.sh
-  echo "3: $(date '+%Y-%m-%d %H:%M:%S')" >>~/scripts/hypr/log.txt
 fi
