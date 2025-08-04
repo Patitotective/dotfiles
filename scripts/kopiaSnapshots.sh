d0b3b9089bed17c197425de89b895be2b3d1cfe6
Cristobal <cristobalriaga@gmail.com>
2025-08-04 13:14:35 -0500

    scripts/kopiaSnapshots: changed directories

--- scripts/kopiaSnapshots.sh
+++ scripts/kopiaSnapshots.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-kopia snapshot create "/usr/share/fonts"
-kopia snapshot create "/usr/local/share/fonts"
 kopia snapshot create "$HOME/Videos"
 kopia snapshot create "$HOME/Pictures"
 kopia snapshot create "$HOME/Music"
-kopia snapshot create "$HOME/Documents"
-kopia snapshot create "$HOME/.local/share/fonts"
+kopia snapshot create "$HOME/Documents/orgfiles"
+kopia snapshot create "$HOME/Documents/crochet"
