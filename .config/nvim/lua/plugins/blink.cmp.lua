4c9c65ce7b17245014a7f7b79bcd73f06a1ac913
Cristobal <cristobalriaga@gmail.com>
2025-08-17 13:05:55 +0200

    nvim/blink.cmp: ignore_root_slash -> true

--- .config/nvim/lua/plugins/blink.cmp.lua
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -9,6 +9,7 @@ return {
         path = {
           opts = {
             show_hidden_files_by_default = true,
+            ignore_root_slash = true, -- Treat `/path` as starting from the current working directory (cwd) instead of the root of your filesystem
           },
         },
       },
