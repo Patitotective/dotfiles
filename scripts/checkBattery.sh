32d5fd3780754798a7d9745aa68e6b952ff1d339
Cristobal <cristobalriaga@gmail.com>
2025-08-17 19:48:04 +0200

    checkBattery: better notifications

--- scripts/checkBattery.sh
+++ scripts/checkBattery.sh
@@ -11,19 +11,19 @@ acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
   fi
 
   if [ "$status" = Discharging ] && [ "$capacity" -le 10 ] && [ ! -f ~/scripts/.10BatteryNotif ]; then
-    notify-send -u critical -a "Power Management (user)" "Low Battery" "Plug your computer to avoid losing any work."
+    dunstify --urgency=critical --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-010.svg" "Low Battery" "Battery is on or below 10%. Charge your computer to avoid losing any work."
     touch ~/scripts/.10BatteryNotif
     mpv --loop=1 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
   elif [ "$status" = Discharging ] && [ "$capacity" -le 40 ] && [ ! -f ~/scripts/.40BatteryNotif ]; then
-    # notify-send -u critical -a "Power Management (user)" "Battery At 40%" "Plug your computer."
+    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-040.svg" "Battery is at or below 40%" "Plug the computer to prolong the battery's life."
     touch ~/scripts/.40BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/outcome-failure.oga
   elif [ "$status" = Charging ] && [ "$capacity" -ge 100 ] && [ ! -f ~/scripts/.100BatteryNotif ]; then
-    # notify-send -u normal -a "Power Management (user)" "Fully Charged" "You may unplug your computer."
+    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-100-charging.svg" "Battery is fully charged" "Unplug the computer to prolong the battery's life."
     touch ~/scripts/.100BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/message-sent-instant.oga
   elif [ "$status" = Charging ] && [ "$capacity" -ge 60 ] && [ ! -f ~/scripts/.60BatteryNotif ]; then
-    # notify-send -u critical -a "Power Management (user)" "Battery At 60%" "Unplug your computer."
+    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-060-charging.svg" "Battery is at or above 60%" "Unplug the computer to prolong the battery's life."
     touch ~/scripts/.60BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/outcome-success.oga
   fi
