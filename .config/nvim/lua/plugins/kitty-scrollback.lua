95e4a2d98c6ebd2dd53da7f4165ee87c42f90a61
Cristobal <cristobalriaga@gmail.com>
2025-08-27 13:44:40 +0200

    nvim/kitty-scrollback: disable paste window

--- .config/nvim/lua/plugins/kitty-scrollback.lua
+++ .config/nvim/lua/plugins/kitty-scrollback.lua
@@ -5,7 +5,11 @@ return {
   event = { "User KittyScrollbackLaunch" },
   -- version = '*', -- latest stable version, may have breaking changes if major version changed
   -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
-  config = function()
-    require("kitty-scrollback").setup()
-  end,
+  opts = {
+    {
+      paste_window = {
+        yank_register_enabled = false, -- this disables the paste window
+      },
+    },
+  },
 }
