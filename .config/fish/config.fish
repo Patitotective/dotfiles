a9479e431b5db6460ff5c6584daaa665d4b606ea
Cristobal <cristobalriaga@gmail.com>
2025-09-04 10:07:55 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -49,3 +49,6 @@ alias ls=eza
 alias jless="jless --relative-line-numbers"
 
 set -x LEDGER_FILE ~/Documents/finance/2025.journal
+
+# if i run this then suddenly it won't show up
+# tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
