af128e0b67c71d01f0ea0d0d6e3a81e1259c6da5
Cristobal <cristobalriaga@gmail.com>
2025-09-08 22:39:57 +0200

    moved LEDGER_FILE to uwsm/env, fixed FOPIA_CHECK_FOR_UPDATES, set nvim as default man pager

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -48,7 +48,5 @@ alias onlyoffice="onlyoffice-desktopeditors --xdg-desktop-portal=default"
 alias ls=eza
 alias jless="jless --relative-line-numbers"
 
-set -x LEDGER_FILE ~/Documents/finance/2025.journal
-
 # if i run this then suddenly it won't show up
 # tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
