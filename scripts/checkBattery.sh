058dd897599ebf69746bc58d31a2e64679ab29ff
Cristobal <cristobalriaga@gmail.com>
2025-09-08 12:00:41 +0200

    fixed icons path

--- scripts/checkBattery.sh
+++ scripts/checkBattery.sh
@@ -11,19 +11,19 @@ acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
   fi
 
   if [ "$status" = Discharging ] && [ "$capacity" -le 10 ] && [ ! -f ~/scripts/.10BatteryNotif ]; then
-    dunstify --urgency=critical --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-010.svg" "Low Battery" "Battery is on or below 10%. Charge your computer to avoid losing any work."
+    dunstify --urgency=critical --icon="/usr/share/icons/Tela-purple/16/panel/battery-010.svg" "Low Battery" "Battery is on or below 10%. Charge your computer to avoid losing any work."
     touch ~/scripts/.10BatteryNotif
     mpv --loop=1 /usr/share/sounds/freedesktop/stereo/suspend-error.oga
   elif [ "$status" = Discharging ] && [ "$capacity" -le 40 ] && [ ! -f ~/scripts/.40BatteryNotif ]; then
-    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-040.svg" "Battery is at or below 40%" "Plug the computer to prolong the battery's life."
+    dunstify --icon="/usr/share/icons/Tela-purple/16/panel/battery-040.svg" "Battery is at or below 40%" "Plug the computer to prolong the battery's life."
     touch ~/scripts/.40BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/outcome-failure.oga
   elif [ "$status" = Charging ] && [ "$capacity" -ge 100 ] && [ ! -f ~/scripts/.100BatteryNotif ]; then
-    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-100-charging.svg" "Battery is fully charged" "Unplug the computer to prolong the battery's life."
+    dunstify --icon="/usr/share/icons/Tela-purple/16/panel/battery-100-charging.svg" "Battery is fully charged" "Unplug the computer to prolong the battery's life."
     touch ~/scripts/.100BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/message-sent-instant.oga
   elif [ "$status" = Charging ] && [ "$capacity" -ge 60 ] && [ ! -f ~/scripts/.60BatteryNotif ]; then
-    dunstify --icon="$HOME/.local/share/icons/Tela-purple/16/panel/battery-060-charging.svg" "Battery is at or above 60%" "Unplug the computer to prolong the battery's life."
+    dunstify --icon="/usr/share/icons/Tela-purple/16/panel/battery-060-charging.svg" "Battery is at or above 60%" "Unplug the computer to prolong the battery's life."
     touch ~/scripts/.60BatteryNotif
     mpv /usr/share/sounds/ocean/stereo/outcome-success.oga
   fi
