ae7c0bdb2bfeefe0e8bf4b82b3b81c27759f7df2
Cristobal <cristobalriaga@gmail.com>
2025-09-09 12:53:31 +0200

    waybar: ooops, infinite recursion consumed too much resources, fixed it

--- scripts/hypr/capslock.fish
+++ scripts/hypr/capslock.fish
@@ -1,6 +1,5 @@
 #!/bin/fish
 set capsLock (hyprctl -j devices | jq '.keyboards.[] | select(.main) | .capsLock')
-kill -SIGRTMIN+3 $(pgrep waybar) # So that waybar updates the icon
 if $capsLock == true
     echo '{"percentage": 100}'
 else
--- scripts/hypr/numlock.fish
+++ scripts/hypr/numlock.fish
@@ -1,6 +1,5 @@
 #!/bin/fish
 set capsLock (hyprctl -j devices | jq '.keyboards.[] | select(.main) | .numLock')
-kill -SIGRTMIN+4 $(pgrep waybar) # So that waybar updates the icon
 if $capsLock == true
     echo '{"percentage": 100}'
 else
