c071d4d2aaa8463651bfa5aac221669d5d4ae699
Cristobal <cristobalriaga@gmail.com>
2025-10-02 19:17:08 +0200

    workaround? since if itunes was focusing some item, space may not pause it or similar

--- scripts/hypr/playPause.fish
+++ scripts/hypr/playPause.fish
@@ -1,5 +1,6 @@
 #!/bin/fish
 if pidof -qx iTunes.exe # if iTunes is running
+    hyprctl dispatch sendshortcut ", esc, class:itunes.exe" # to close any menus or so
     hyprctl dispatch sendshortcut ", space, class:itunes.exe"
 else
     playerctl play-pause
--- scripts/hypr/playPrev.fish
+++ scripts/hypr/playPrev.fish
@@ -1,5 +1,6 @@
 #!/bin/fish
 if pidof -qx iTunes.exe # if iTunes is running
+    hyprctl dispatch sendshortcut ", esc, class:itunes.exe" # to close any menus or so
     hyprctl dispatch sendshortcut "ctrl, left, class:itunes.exe"
 else
     playerctl previous
