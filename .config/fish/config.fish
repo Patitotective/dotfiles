352f62e4d2adf109d2f77f4fd6ea2a8cc8ffb41a
Cristobal <cristobalriaga@gmail.com>
2025-10-02 19:16:36 +0200

    fish: use vim default mode by default instead of insert

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -50,3 +50,7 @@ alias jless="jless --relative-line-numbers"
 
 # if i run this then suddenly it won't show up
 # tide configure --auto --style=Lean --prompt_colors='True color' --show_time='24-hour format' --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+
+function back_to_normal --on-event fish_prompt
+    fish_vi_key_bindings default
+end
