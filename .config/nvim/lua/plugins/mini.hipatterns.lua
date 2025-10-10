eb6415b6aa08572845b4016e5e587005cee814eb
Cristobal <cristobalriaga@gmail.com>
2025-10-10 23:36:02 +0200

    nvim/mini.hipatterns: six hex rgb pattern

--- .config/nvim/lua/plugins/mini.hipatterns.lua
+++ .config/nvim/lua/plugins/mini.hipatterns.lua
@@ -7,7 +7,7 @@ return {
   opts = {
     highlighters = {
       rgba_color_hashless = {
-        pattern = "%f[%x]()%x%x%x%x%x%x%x%x()%f[%X]", -- To match RGBA colors without hash
+        pattern = "%f[%x]()%x%x%x%x%x%x%x%x()%f[%X]", -- To match eight digit hex rgba colors
         group = function(buf_id, _, data)
           local path = vim.api.nvim_buf_get_name(buf_id)
           if path:endswith("ly/config.ini") then
@@ -19,27 +19,26 @@ return {
         extmark_opts = { priority = 2000 },
       },
       rgba_color = {
-        pattern = "rgba%(()%x%x%x%x%x%x%x%x()%)", -- To match RGBA colors without hash
+        pattern = "rgba%(()%x%x%x%x%x%x%x%x()%)", -- To match the same as above but surrounded by rgba()
         group = function(_, _, data)
           return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(6, 11), "bg")
         end,
         extmark_opts = { priority = 2000 },
       },
       rgb_color = {
-        pattern = "rgb%(()%x%x%x%x%x%x()%)", -- To match RGBA colors without hash
+        pattern = "rgb%(()%x%x%x%x%x%x()%)", -- Same as above but only rgb()
         group = function(_, _, data)
           return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(5, 10), "bg")
         end,
         extmark_opts = { priority = 2000 },
       },
-      -- Both patterns match :/
-      -- hex_color_hashless = {
-      --   pattern = "()%x%x%x%x%x%x()%f[%X]", -- To match RGBA colors without hash
-      --   group = function(_, _, data)
-      --     return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(1, 6), "bg")
-      --   end,
-      --   extmark_opts = { priority = 1000 },
-      -- },
+      rgb_color_hashless = {
+        pattern = "%f[%x]()%x%x%x%x%x%x()%f[%X]", -- To match RGB colors without hash
+        group = function(_, _, data)
+          return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(1, 6), "bg")
+        end,
+        extmark_opts = { priority = 1000 },
+      },
     },
   },
 }
