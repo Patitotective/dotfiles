a45aa0f8425478593d57a202f2cd60e2a2ccba30
Cristobal <cristobalriaga@gmail.com>
2025-10-27 23:46:49 +0100

    nvim/mini.hipatterns: fixed errors

--- .config/nvim/lua/plugins/mini.hipatterns.lua
+++ .config/nvim/lua/plugins/mini.hipatterns.lua
@@ -32,7 +32,9 @@ return {
           end
           -- NOTE: for some reason using data.from_col and data_to_col doesn't work here
           local match = data.full_match:sub(1, 6)
-          assert(#match == 6)
+          if #match ~= 6 then
+            return
+          end
           local path = vim.api.nvim_buf_get_name(buf_id)
           if endswith(path, "ly/config.ini") then
             return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(3, 8), "bg")
@@ -44,35 +46,42 @@ return {
       },
       -- TODO: make it work for comma separated 0..1 values
       rgb_color_255 = {
-        pattern = "rgb%(()%d%d?%d?%s*,%s*%d%d?%d?%s*,%s*%d%d?%d?()%)",
+        pattern = "rgb%(%d%d?%d?%s*,%s*%d%d?%d?%s*,%s*%d%d?%d?%)",
         group = function(_, _, data)
-          local match = data.full_match:sub(data.from_col, data.to_col)
+          local match = data.full_match:sub(5, -2):gsub("%s+", "")
           local splitted = split(match, ",")
-          assert(#splitted == 3)
-          local r = splitted[1]:gsub("%s+", "")
-          local g = splitted[2]:gsub("%s+", "")
-          local b = splitted[3]:gsub("%s+", "")
-          local rx = string.format("%x", r)
-          local gx = string.format("%x", g)
-          local bx = string.format("%x", b)
-          assert(#rx == 2 and #gx == 2 and #bx == 2)
+          if #splitted ~= 3 then
+            return
+          end
+
+          local rx = string.format("%02x", splitted[1])
+          local gx = string.format("%02x", splitted[2])
+          local bx = string.format("%02x", splitted[3])
+
+          if #rx ~= 2 or #gx ~= 2 or #bx ~= 2 then
+            return
+          end
           return require("mini.hipatterns").compute_hex_color_group("#" .. rx .. gx .. bx, "bg")
         end,
         extmark_opts = { priority = 2000 },
       },
       rgba_color_255 = {
-        pattern = "rgba%(()%d%d?%d?%s*,%s*%d%d?%d?%s*,%s*%d%d?%d?,%s*%d%.?%d*()%)",
+        pattern = "rgba%(%d%d?%d?%s*,%s*%d%d?%d?%s*,%s*%d%d?%d?,%s*%d%.?%d*%)",
         group = function(_, _, data)
-          local match = data.full_match:sub(data.from_col, data.to_col)
+          local match = data.full_match:sub(6, -2):gsub("%s+", "")
+          -- vim.notify(match)
           local splitted = split(match, ",")
-          assert(#splitted == 4)
-          local r = splitted[1]:gsub("%s+", "")
-          local g = splitted[2]:gsub("%s+", "")
-          local b = splitted[3]:gsub("%s+", "")
-          local rx = string.format("%x", r)
-          local gx = string.format("%x", g)
-          local bx = string.format("%x", b)
-          assert(#rx == 2 and #gx == 2 and #bx == 2)
+          if #splitted ~= 4 then
+            return
+          end
+
+          local rx = string.format("%02x", splitted[1])
+          local gx = string.format("%02x", splitted[2])
+          local bx = string.format("%02x", splitted[3])
+
+          if #rx ~= 2 or #gx ~= 2 or #bx ~= 2 then
+            return
+          end
           return require("mini.hipatterns").compute_hex_color_group("#" .. rx .. gx .. bx, "bg")
         end,
         extmark_opts = { priority = 2000 },
@@ -80,20 +89,22 @@ return {
       rgba_color = {
         pattern = "rgba%(()%x%x%x%x%x%x()%x%x%)", -- To match the same as above but surrounded by rgba()
         group = function(_, _, data)
-          return require("mini.hipatterns").compute_hex_color_group(
-            "#" .. data.full_match:sub(data.from_col, data.to_col),
-            "bg"
-          )
+          local match = data.full_match:sub(data.from_col, data.to_col)
+          if #match == 0 then
+            return
+          end
+          return require("mini.hipatterns").compute_hex_color_group("#" .. match, "bg")
         end,
         extmark_opts = { priority = 2000 },
       },
       rgb_color = {
         pattern = "rgb%(()%x%x%x%x%x%x()%)", -- Same as above but only rgb()
         group = function(_, _, data)
-          return require("mini.hipatterns").compute_hex_color_group(
-            "#" .. data.full_match:sub(data.from_col, data.to_col),
-            "bg"
-          )
+          local match = data.full_match:sub(data.from_col, data.to_col)
+          if #match == 0 then
+            return
+          end
+          return require("mini.hipatterns").compute_hex_color_group("#" .. match, "bg")
         end,
         extmark_opts = { priority = 2000 },
       },
@@ -106,7 +117,9 @@ return {
           -- NOTE: for some reason using data.from_col and data_to_col doesn't work here
           -- as it returns some numbers over 70
           local match = data.full_match:sub(1, 6)
-          assert(#match == 6)
+          if #match ~= 6 then
+            return
+          end
           return require("mini.hipatterns").compute_hex_color_group("#" .. data.full_match:sub(1, 6), "bg")
         end,
         extmark_opts = { priority = 1000 },
