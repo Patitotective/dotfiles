7615ccc20358ba335c43c59c6afdad8018034b55
Cristobal <cristobalriaga@gmail.com>
2025-11-12 11:21:36 -0500

    nvim: new lsp bindings

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,7 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "<c-k>", false, mode = { "i" } } -- disable c-k in insert mode
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<c-k>", false, mode = { "i" } }, -- disable c-k in insert mode
+        },
+      },
+    },
+  },
 }
