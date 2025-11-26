f6af03651d773bf963036b7a9df702b05f8a96ce
Cristobal <cristobalriaga@gmail.com>
2025-11-26 11:02:04 -0500

    nvim/fzf: changed find files bindings

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -24,5 +24,20 @@ return {
     { "<leader>s~", "<cmd>FzfLua files cwd=~<cr>", desc = "Search files in $HOME" },
     { "<leader>s/", "<cmd>FzfLua files cwd=/<cr>", desc = "Search files in /" },
     { "<leader>fc", "<cmd>FzfLua files cwd=~/.config/<cr>", desc = "Search config files" },
+    {
+      "<leader><space>",
+      function()
+        LazyVim.pick.open("files", { cwd = vim.fn.getcwd() })
+      end,
+      desc = "Find Files (Root Dir)",
+    },
+    {
+      "<leader>ff",
+      function()
+        LazyVim.pick.open("files", { cwd = vim.fn.expand("%:p:h") })
+      end,
+      desc = "Find Files (cwd)",
+    },
+    { "<leader>fF", false },
   },
 }
