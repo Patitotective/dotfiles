cf2c00e372dcda9b8c1ab96dee813e5df6970d98
Cristobal <cristobalriaga@gmail.com>
2025-12-02 16:56:59 -0500

    nvim/yazi

--- .config/nvim/lua/plugins/yazi.lua
+++ .config/nvim/lua/plugins/yazi.lua
@@ -28,6 +28,16 @@ return {
     open_for_directories = false,
     keymaps = {
       show_help = "<f1>",
+      open_file_in_vertical_split = false, -- "<c-v>",
+      open_file_in_horizontal_split = false, -- "<c-x>",
+      open_file_in_tab = false, -- "<c-t>",
+      grep_in_directory = false, -- "<c-s>",
+      replace_in_directory = false, -- "<c-g>",
+      cycle_open_buffers = false, -- "<tab>",
+      copy_relative_path_to_selected_files = false, -- "<c-y>",
+      send_to_quickfix_list = false, -- "<c-q>",
+      change_working_directory = false, -- "<c-\\>",
+      open_and_pick_window = false, -- "<c-o>",
     },
   },
   -- 👇 if you use `open_for_directories=true`, this is recommended
