226aca7ab7ab50a752c61aa33f8c352ed06c6e5a
Cristobal <cristobalriaga@gmail.com>
2025-12-19 17:57:00 -0500

    just use c-v

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 set -g fish_key_bindings fish_vi_key_bindings
 
 bind yy fish_clipboard_copy
-bind p fish_clipboard_paste
+# bind p fish_clipboard_paste
 # bind P fish_clipboard_paste # TODO: why does this not work? 😭
 
 bind -M insert ctrl-w backward-kill-word
