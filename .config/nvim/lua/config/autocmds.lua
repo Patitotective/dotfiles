74179971786613a537d8c758379c821404f29ef8
Cristobal <cristobalriaga@gmail.com>
2026-01-13 20:47:17 -0500

    organizing

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -273,15 +273,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.keymap.set("n", "q", "<esc>", { buffer = ctx.buf, remap = true })
   end,
 })
-vim.api.nvim_create_user_command("OverseerRestartLast", function()
-  local overseer = require("overseer")
-  local tasks = overseer.list_tasks({ recent_first = true })
-  if vim.tbl_isempty(tasks) then
-    vim.notify("No tasks found", vim.log.levels.WARN)
-  else
-    overseer.run_action(tasks[1], "restart")
-  end
-end, {})
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "aerial",
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -8,3 +8,21 @@ vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
   filename = { ["env-hyprland"] = "conf" },
 })
+
+vim.api.nvim_create_user_command("OverseerRestartLast", function()
+  local overseer = require("overseer")
+  local tasks = overseer.list_tasks({ recent_first = true })
+  if vim.tbl_isempty(tasks) then
+    vim.notify("No tasks found", vim.log.levels.WARN)
+  else
+    overseer.run_action(tasks[1], "restart")
+  end
+end, {})
+
+vim.api.nvim_create_user_command("LspStart", function(args)
+  vim.lsp.enable(args.fargs[1]) -- Enable the specified server
+end, { nargs = "?" })
+
+vim.api.nvim_create_user_command("LspStop", function(args)
+  vim.lsp.disable(args.fargs[1]) -- Disable the specified server
+end, { nargs = "?" })
