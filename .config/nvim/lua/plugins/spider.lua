cba64e338fac75135a3964ba4d1f82263b5e89bd
Cristobal <cristobalriaga@gmail.com>
2026-03-09 11:52:00 +0100

    hmm

--- .config/nvim/lua/plugins/spider.lua
+++ .config/nvim/lua/plugins/spider.lua
@@ -1,37 +1,55 @@
+-- a bC --
+-- á bC --
 return {
-  "chrisgrieser/nvim-spider",
-  enabled = false,
-  opts = {},
-  keys = {
-    {
-      "w",
-      "<cmd>lua require('spider').motion('w')<CR>",
-      mode = { "n", "o", "x" },
-      desc = "Move to start of next of word",
-    },
-    {
-      "e",
-      "<cmd>lua require('spider').motion('e')<CR>",
-      mode = { "n", "o", "x" },
-      desc = "Move to end of word",
-    },
-    {
-      "b",
-      "<cmd>lua require('spider').motion('b')<CR>",
-      mode = { "n", "o", "x" },
-      desc = "Move to start of previous word",
-    },
+  {
+    "chrisgrieser/nvim-spider",
+    enabled = true,
+    lazy = false,
+    opts = {},
+    -- TODO: recognize when the next non-whitespace character is going to be a non-ascii character to use extend_word_motion instead of spider
+    -- dependencies = {
+    --   "uga-rosa/utf8.nvim",
+    -- },
+    -- keys = {
+    --   {
+    --     "w",
+    --     -- "<cmd>lua require('spider').motion('w')<CR>",
+    --     function()
+    --       local region = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("."))
+    --       local utf8 = require("utf8")
+    --       if #region < 1 then
+    --         return
+    --       end
+    --       vim.notify_once("a " .. region[1] .. utf8.codepoint(region[1]))
+    --       if utf8.codepoint(region[1]) > 126 then
+    --         require("extend_word_motion").handle_motion("w")
+    --       else
+    --         require("spider").motion("w")
+    --       end
+    --     end,
+    --     mode = { "n", "o", "x" },
+    --     desc = "Move to start of next of word",
+    --   },
+    --   {
+    --     "e",
+    --     "<cmd>lua require('spider').motion('e')<CR>",
+    --     mode = { "n", "o", "x" },
+    --     desc = "Move to end of word",
+    --   },
+    --   {
+    --     "b",
+    --     "<cmd>lua require('spider').motion('b')<CR>",
+    --     mode = { "n", "o", "x" },
+    --     desc = "Move to start of previous word",
+    --   },
+    -- },
   },
-  dependencies = {
-    { "rami3l/nvim-spider-utf8", build = "rockspec" },
-  },
-  -- dependencies = {
-  --   "theHamsta/nvim_rocks",
-  --   -- build = "pip3 install --user hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-  --   -- build = "yay -S hererocks && python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-  --   build = "python3 -mhererocks . -j2.1.0-beta3 -r3.0.0 && cp nvim_rocks.lua lua",
-  --   config = function()
-  --     require("nvim_rocks").ensure_installed("luautf8")
-  --   end,
+  -- {
+  --   "vhyrro/luarocks.nvim",
+  --   priority = 1000, -- high priority required, luarocks.nvim should run as the first plugin in your config
+  --   lazy = false,
+  --   opts = {
+  --     rocks = { "luautf8 ~> 0.1.5" }, -- specifies a list of rocks to install
+  --   },
   -- },
 }
