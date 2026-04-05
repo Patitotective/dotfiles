7b29220f2c41bfa4f2871f69d84adeb69b8f2590
Cristobal <cristobalriaga@gmail.com>
2026-04-05 13:02:30 +0200

    scripts

--- scripts/ankiImport.py
+++ scripts/ankiImport.py
@@ -9,15 +9,14 @@ col_path = os.path.join(anki_home, "collection.anki2")
 col = Collection(col_path)
 
 try:
-    path = sys.argv[1]
+    for path in sys.argv[1:]:
+        metadata = col.get_csv_metadata(path=path, delimiter=None)
+        request = ImportCsvRequest(path=path, metadata=metadata)
+        response = col.import_csv(request)
 
-    metadata = col.get_csv_metadata(path=path, delimiter=None)
-    request = ImportCsvRequest(path=path, metadata=metadata)
-    response = col.import_csv(request)
-
-    print(f"Found: {response.log.found_notes}")
-    print(f"Updated: {len(response.log.updated)}")
-    print(f"New: {len(response.log.new)}")
-
+        print(f"Importing: {os.path.relpath(path)}")
+        print(f"Found: {response.log.found_notes}")
+        print(f"Updated: {len(response.log.updated)}")
+        print(f"New: {len(response.log.new)}\n")
 finally:
     col.close()
