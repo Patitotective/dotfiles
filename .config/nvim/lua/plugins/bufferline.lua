5c21563e02c6bb435bc8947c8334736f21c111db
Cristobal <cristobalriaga@gmail.com>
2026-09-14 19:03:04 +0200

    nvim

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -8,12 +8,10 @@ return {
   "akinsho/bufferline.nvim",
   opts = {
     options = {
+      move_wraps_at_ends = true,
       indicator = {
         style = "none",
-        -- icon = " ▐",
       },
-      -- separator_style = "thin",
-      -- show_close_icon = false,
       hover = {
         enabled = true,
         delay = 00,
@@ -29,33 +27,100 @@ return {
     {
       "L",
       function()
-        repeatCmd("BufferLineCycleNext", vim.v.count1)
+        require("bufferline").cycle(vim.v.count1)
       end,
       desc = "Next buffer",
     },
     {
       "H",
       function()
-        -- vim.cmd("bprev " .. vim.v.count1)
-        repeatCmd("BufferLineCyclePrev", vim.v.count1)
+        require("bufferline").cycle(-vim.v.count1)
       end,
       desc = "Previous buffer",
     },
     {
-      "]b",
+      "]B",
       function()
-        -- vim.cmd("bnext " .. vim.v.count1)
-        repeatCmd("BufferLineCycleNext", vim.v.count1)
+        repeatCmd("BufferLineMoveNext", vim.v.count1)
       end,
       desc = "Next buffer",
     },
     {
-      "[b",
+      "[B",
       function()
-        -- vim.cmd("bprev " .. vim.v.count1)
-        repeatCmd("BufferLineCyclePrev", vim.v.count1)
+        repeatCmd("BufferLineMovePrev", vim.v.count1)
       end,
       desc = "Previous buffer",
+    },
+    {
+      "<leader>1",
+      function()
+        require("bufferline").go_to(1, true)
+      end,
+      desc = "Go to 1st buffer",
+    },
+    {
+      "<leader>2",
+      function()
+        require("bufferline").go_to(2, true)
+      end,
+      desc = "Go to 2nd buffer",
+    },
+    {
+      "<leader>3",
+      function()
+        require("bufferline").go_to(3, true)
+      end,
+      desc = "Go to 3rd buffer",
+    },
+    {
+      "<leader>4",
+      function()
+        require("bufferline").go_to(4, true)
+      end,
+      desc = "Go to 4th buffer",
+    },
+    {
+      "<leader>5",
+      function()
+        require("bufferline").go_to(5, true)
+      end,
+      desc = "Go to 5th buffer",
+    },
+    {
+      "<leader>6",
+      function()
+        require("bufferline").go_to(6, true)
+      end,
+      desc = "Go to 6th buffer",
+    },
+    {
+      "<leader>7",
+      function()
+        require("bufferline").go_to(7, true)
+      end,
+      desc = "Go to 7th buffer",
+    },
+    {
+      "<leader>8",
+      function()
+        require("bufferline").go_to(8, true)
+      end,
+      desc = "Go to 8th buffer",
+    },
+    {
+      "<leader>9",
+      function()
+        require("bufferline").go_to(9, true)
+      end,
+      desc = "Go to 9th buffer",
+    },
+    {
+      "<leader>0",
+      function()
+        require("bufferline").go_to(10, true)
+      end,
+      desc = "Go to 10th buffer",
     },
   },
 }
