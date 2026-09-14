9f5f5305f4f5903bc48b0b3ec78d5ad1f8aa04db
Cristobal <cristobalriaga@gmail.com>
2026-09-14 19:03:29 +0200

    yazi

--- .config/yazi/plugins/folder-rules.yazi/main.lua
+++ .config/yazi/plugins/folder-rules.yazi/main.lua
@@ -2,11 +2,11 @@ local function setup()
 	ps.sub("cd", function()
 		local cwd = cx.active.current.cwd
 		if cwd:ends_with("Downloads") then
-			ya.mgr_emit("sort", { "mtime", reverse = true, dir_first = false })
+			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
 		elseif cwd:contains("dev/") then
-			ya.mgr_emit("sort", { "mtime", reverse = true, dir_first = true })
+			ya.emit("sort", { "mtime", reverse = true, dir_first = true })
 		else
-			ya.mgr_emit("sort", { "natural", reverse = false, dir_first = true, sensitive = true, translit = true })
+			ya.emit("sort", { "natural", reverse = false, dir_first = true, sensitive = true, translit = true })
 		end
 	end)
 end
